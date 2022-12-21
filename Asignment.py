8cdbae57fb6265fdeba491dd5d5fbfff76dc2d5b
Matecast <121044084+Matecast@users.noreply.github.com>
2022-12-21 10:22:58 -0300

    Trying

---
+++ Asignment.py
@@ -0,0 +1,78 @@
+{
+  "metadata": {
+    "language_info": {
+      "codemirror_mode": {
+        "name": "python",
+        "version": 3
+      },
+      "file_extension": ".py",
+      "mimetype": "text/x-python",
+      "name": "python",
+      "nbconvert_exporter": "python",
+      "pygments_lexer": "ipython3",
+      "version": "3.8"
+    },
+    "kernelspec": {
+      "name": "python",
+      "display_name": "Pyolite",
+      "language": "python"
+    }
+  },
+  "nbformat_minor": 4,
+  "nbformat": 4,
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "source": "# My Jupyter Notebook on IBM Watson Studio",
+      "metadata": {}
+    },
+    {
+      "cell_type": "markdown",
+      "source": "**Mateo Castori**\nData scientist in astronomy field",
+      "metadata": {}
+    },
+    {
+      "cell_type": "markdown",
+      "source": "_I am interested in data science because I want to study astronomy from another point of view and try to uncover the mysteries of the Universe._",
+      "metadata": {}
+    },
+    {
+      "cell_type": "markdown",
+      "source": "### My code in the line below is supposed to make a simple nice math equation",
+      "metadata": {}
+    },
+    {
+      "cell_type": "code",
+      "source": "69+420",
+      "metadata": {
+        "trusted": true
+      },
+      "execution_count": 20,
+      "outputs": [
+        {
+          "execution_count": 20,
+          "output_type": "execute_result",
+          "data": {
+            "text/plain": "489"
+          },
+          "metadata": {}
+        }
+      ]
+    },
+    {
+      "cell_type": "markdown",
+      "source": "[![IMAGE ALT TEXT HERE](http://img.youtube.com/vi/YOUTUBE_VIDEO_ID_HERE/0.jpg)](https://www.youtube.com/watch?v=dQw4w9WgXcQ)",
+      "metadata": {}
+    },
+    {
+      "cell_type": "markdown",
+      "source": "_I should really write something here but I don´t know what._\n\nMarkdown | Code | Raw\n--- | --- | ---\n*Argentina* | **_World_** | **Champion**\n1-1978 | 2-1986 | 3-2022",
+      "metadata": {}
+    },
+    {
+      "cell_type": "markdown",
+      "source": "# Messi\n\n---\n\n## Greatest\n\n***\n\n### of\n\n___\n\n#### All \n\n___\n\n##### Time",
+      "metadata": {}
+    }
+  ]
+}
